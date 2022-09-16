306c90d3d31bb740350b188d4ddc02bca59d9ef2
matheus.moraes <matheus.pozzer@edu.pucrs.br>
2022-09-16 16:43:59 -0300

    Adicionando probabilidade e concertando bugs do codigo

--- src/FilaSimples.java
+++ src/FilaSimples.java
@@ -9,6 +9,7 @@ public class FilaSimples {
     CongruenteLinear cl = new CongruenteLinear();
     static int contador = 0;
     ArrayList<Float> agenda_chegada = new ArrayList<>();
+    ArrayList<Float> chegada_fila = new ArrayList<>();
     ArrayList<Float> agenda_saida = new ArrayList<>();
     private float[] tempo_por_quantidade;
     private float tempo_global = 0;
@@ -83,24 +84,36 @@ public class FilaSimples {
         this.fila = numero;
     }
 
+    public void agendarProximaChegada(){
+        if(agenda_chegada.size() >= 1){
+            agenda_chegada.remove(0);
+        }
+        float proxima_chegada = T + gerarRandom(intervalo_chegada);
+        setProximaChegada(proxima_chegada);
+        agenda_chegada.add(proxima_chegada);
+    }
+
     public void chegada(float T) {
         setT(T);
+        setTempoGlobal(T);
         float tempoAnteriorChegada = getTempoAnteriorChegada();
-        setTempoAnteriorChegada(T);
-        if (fila <= K) {
+        if (fila < K) {
             fila++;
-            tempo_por_quantidade[qntd_clientes] += ( T - Math.max(tempoAnteriorChegada, getTempoAnteriorSaida()));
+            tempo_por_quantidade[qntd_clientes] += (T - Math.max(tempoAnteriorChegada, getTempoAnteriorSaida()));
             qntd_clientes++;
-            printaTempoTotalPorQuantidade();
-            setTempoGlobal(T);
+            chegada_fila.add(T);
+            setTempoAnteriorChegada(T);
+            // printaTempoTotalPorQuantidade();
             if (fila <= servidores) {
                 float proxima_saida = T + gerarRandom(intervalo_atendimento);
-                // setProximaSaida(aux);
                 agenda_saida.add(proxima_saida);
             }
-            float proxima_chegada = T + gerarRandom(intervalo_chegada);
-            setProximaChegada(proxima_chegada);
-            agenda_chegada.add(proxima_chegada);
+            agendarProximaChegada();
+        } else {
+            agendarProximaChegada();
+            // float proxima_chegada = T + gerarRandom(intervalo_chegada);
+            // setProximaChegada(proxima_chegada);
+            // agenda_chegada.add(proxima_chegada);
         }
     }
 
@@ -108,18 +121,17 @@ public class FilaSimples {
         float aux_tempo;
         setT(T);
         aux_tempo = T;
-        tempo_por_quantidade[qntd_clientes] += ( T - Math.max(getTempoAnteriorChegada(),getTempoAnteriorSaida()));
-        System.out.println(agenda_chegada.size() + "tamanhooo");
+        tempo_por_quantidade[qntd_clientes] += (T - Math.max(getTempoAnteriorChegada(), getTempoAnteriorSaida()));
         qntd_clientes--;
-        System.out.println("TEMPO SAIDA: TESTE" + Math.max(getTempoAnteriorChegada(),getTempoAnteriorSaida()) + " -- " + T);
-        System.out.println("VAPO"+ fila);
+        // System.out.println(
+        //         "TEMPO SAIDA: TESTE" + Math.max(getTempoAnteriorChegada(), getTempoAnteriorSaida()) + " -- " + T);
         setTempoAnteriorSaida(aux_tempo);
         setTempoGlobal(T);
         fila--;
-        agenda_chegada.remove(0);
+        chegada_fila.remove(0);
         agenda_saida.remove(0);
-        printaTempoTotalPorQuantidade();
-        if (fila >= servidores) {
+        // printaTempoTotalPorQuantidade();
+        if (fila >= servidores) { 
             agenda_saida.add(T + gerarRandom(intervalo_atendimento));
         }
     }
@@ -129,6 +141,9 @@ public class FilaSimples {
         String[] split = intervalo.split(",");
         int A = Integer.parseInt(split[0]);
         int B = Integer.parseInt(split[1]);
+        if(numeros_random.size() == 0){
+            return 0;
+        }
         U = (B - A) * numeros_random.get(0) + A;
         numeros_random.remove(0);
         return U;
@@ -138,8 +153,8 @@ public class FilaSimples {
         this.agenda_chegada.add(chegada);
     }
 
-    public void AgendaChegadaToString() {
-        for (float chegada : agenda_chegada) {
+    public void chegadaFilaToString() {
+        for (float chegada : this.chegada_fila) {
             System.out.println("CHEGADA: " + chegada);
         }
     }
@@ -154,7 +169,15 @@ public class FilaSimples {
         int index = 0;
         for (float tempo_por_quantidade : tempo_por_quantidade) {
             System.out.println("Tempo total com " + index + " clientes na fila: " + tempo_por_quantidade);
-            index ++;
+            index++;
+        }
+    }
+
+    public void printaProbabilidadePorQntdClientes() {
+        int index = 0;
+        for (float tempo_por_quantidade : tempo_por_quantidade) {
+            System.out.println("Probabilidade de " + index + " clientes na fila: " + ((tempo_por_quantidade/tempo_global) * 100) + "%");
+            index++;
         }
     }
 
@@ -168,16 +191,14 @@ public class FilaSimples {
             System.out.println("Tempo Global: " + tempo_global);
             System.out.println("Ultima chegada: " + getTempoAnteriorChegada());
             System.out.println("Ultima saída: " + getTempoAnteriorSaida());
-            AgendaChegadaToString();
+            chegadaFilaToString();
             AgendaSaidaToString();
             System.out.println("--------------------");
             // System.out.println(getFila());
             if (verificaSaida() == true) {
                 saida(agenda_saida.get(0));
             } else {
-                if (getFila() < this.K) {
-                    chegada(getProximaChegada());
-                }
+                chegada(getProximaChegada());
                 /*
                  * else {
                  * float aux = agenda_chegada.get(agenda_chegada.indexOf(getProximaChegada()) +
@@ -187,8 +208,10 @@ public class FilaSimples {
                  */
             }
         }
-        System.out.println(tempo_global + "tempo total");
-        // printaTempoTotalPorQuantidade();
+        System.out.println(tempo_global + " tempo total");
+        printaTempoTotalPorQuantidade();
+        System.out.println("--------------------");
+        printaProbabilidadePorQntdClientes();
     }
 
     public boolean agendaChegadaIsEmpty() {
@@ -199,12 +222,12 @@ public class FilaSimples {
     }
 
     public boolean verificaSaida() {
-        if (agendaChegadaIsEmpty() == false) {
+        if (agendaChegadaIsEmpty() == false && agenda_saida.size() > 0) {
             if (getProximaChegada() > agenda_saida.get(0)) {
                 return true;
             }
         } else {
-            return true;
+            return false;
         }
         return false;
     }
